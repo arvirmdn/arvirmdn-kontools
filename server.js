fc98ffd6eeac9dd95023dc9af392bbbdb6549a7e
arvirmdn <arviistecu@gmail.com>
2026-09-04 07:13:10 +0700

    Add files via upload

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Serve static files
+app.use(express.static(path.join(__dirname)));
+
+// Fallback ke index.html untuk SPA routing
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`arvirmdn kontools v0.1 running on port ${PORT}`);
+});
